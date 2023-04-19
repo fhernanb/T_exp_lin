5806a9c82cb403dfec57270be78740e8308e9f11
fhernanb <fhernanb@unal.edu.co>
2023-04-19 06:15:03 -0500

    Minor changes in the examples

--- Scripts/Examples for exp_lin_test.R
+++ Scripts/Examples for exp_lin_test.R
@@ -5,8 +5,8 @@ library(RelDists)
 # Example 1
 # Case exponential
 mu <- 1/3
-n <- 500
-set.seed(12345)
+n <- 50
+set.seed(123456)
 y <- rEXP(n=n, mu=mu)
 
 exp_lin_test(y, H0="Data come from exponential")
@@ -14,8 +14,8 @@ exp_lin_test(y, H0="Data come from exponential")
 # Example 2
 # Case Lindley
 mu <- 3
-n <- 500
-set.seed(12345)
+n <- 50
+set.seed(123)
 y <- rLIN(n=n, mu=mu)
 
 exp_lin_test(y, H0="Data come from Lindley")
